33d76a61a0113fe5e8031a1393445a33a4081dc5
Saurav <sauravdeshmukh53@gmail,com>
2023-02-02 15:14:26 +0530

    first commit

--- Branch3.java
+++ Branch3.java
@@ -1,3 +1,33 @@
-public class Branch3 {
-    
+import java.util.*;
+class Branch3{
+	Node head;
+	static class Node{
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+			this.next=null;
+		}
+	}
+	
+	
+	
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		Branch3 s1=new Branch3();
+		s1.head=new Node(10);
+		Node second=new Node(20);
+		Node third=new Node(30);
+		Node fourth=new Node(40);
+		
+		s1.head.next=second;
+		second.next=third;
+		third.next=fourth;
+		
+		
+		
+		
+	}
 }
