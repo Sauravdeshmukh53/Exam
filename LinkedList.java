507e4b77515e30a6c9b34608267b9e1ae45cb25e
Saurav <sauravdeshmukh53@gmail,com>
2023-02-02 15:11:10 +0530

    second commit

--- LinkedList.java
+++ LinkedList.java
@@ -25,15 +25,7 @@ class LinkList1{
 		n.next=newnode;
 	}
 	
-	void display(){
-		Node n=head;
-		while(n!=null){
-			System.out.print(n.data+" ");
-			n=n.next;
-		}
-		System.out.print("null");
-		System.out.println();
-	}
+	
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
@@ -47,12 +39,7 @@ class LinkList1{
 		second.next=third;
 		third.next=fourth;
 		
-		System.out.println("Before inserting list is :");
-		s1.display();
-		
-		System.out.println("After inserting list in end :");
 		s1.insertend(n);
-		s1.display();
 		
 		
 	}
